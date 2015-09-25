0b1e005e9ec4a15469200a9f1f37ac4f0ac44d18
NixonK <nixonkwok@gmail.com>
2015-09-25 10:15:38 -0700

    fixed spacing

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -54,6 +54,8 @@ public class GitLab implements EntryPoint {
 			flexTable.setText(0,0, "Name");
 			
 			flexTable.setText(0,1,"Language");
+			
+			
 			flexTable.setText(0,2, "Shopping Cart Size");
 			flexTable.setText(0,3, "Wish List Size");
 			flexTable.setStyleName("centered-table", true);
