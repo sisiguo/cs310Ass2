0974e8e668d706b64d113adb50bcbe610d1bb4ef
Sisi Guo <sisiguo@Sisis-MacBook-Pro.local>
2015-09-24 22:43:54 -0700

    Initialize project import

---
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -0,0 +1,83 @@
+package ca.ubc.cpsc310.gitlab.client;
+
+import java.util.List;
+
+import ca.ubc.cpsc310.gitlab.client.service.LoadUsersService;
+import ca.ubc.cpsc310.gitlab.client.service.LoadUsersServiceAsync;
+import ca.ubc.cpsc310.gitlab.client.user.IUser;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GitLab implements EntryPoint {
+	private final FlexTable flexTable = new FlexTable();
+
+	final LoadUsersServiceAsync service = GWT.create(LoadUsersService.class);
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() 
+	{
+		
+		service.getUsers(new AsyncCallback<List<IUser>>(){
+
+			@Override
+			public void onFailure(Throwable caught) {
+					Window.alert("Error occured " + caught.getClass() + " : " + caught.getMessage());
+				
+			}
+
+			@Override
+			public void onSuccess(List<IUser> result) {
+				displayUsers(result);
+			}});
+	}
+	
+	/**
+	 * Used to display users 
+	 * @param users
+	 */
+	public void displayUsers(List<IUser> users)
+	{
+
+		RootPanel.get("root").add(flexTable);
+		
+		flexTable.setText(0,0, "Name");
+		
+		flexTable.setText(0,1,"Language");
+		flexTable.setText(0,2, "Shopping Cart Size");
+		flexTable.setText(0,3, "Wish List Size");
+		flexTable.setStyleName("centered-table", true);
+		
+		for(int i=0; i < users.size(); i++)
+		{
+		
+			IUser user = users.get(i);
+			
+			flexTable.setText(i+1,0,user.getName());
+			if(user.getLanguage().trim().equals("EN"))
+			{
+				flexTable.setWidget(i+1, 1, new Image("uk.png"));
+			} else if (user.getLanguage().trim().equals("FR"))
+			{
+				flexTable.setWidget(i+1, 1, new Image("fr.png"));
+			} else
+			{
+				flexTable.setText(i+1,1,user.getLanguage());
+			}
+			
+			flexTable.setText(i+1,2,String.valueOf(user.getShoppingCart().size()));
+			
+			flexTable.setText(i+1,3,String.valueOf(user.getWishList().size()));
+		}
+	}
+}
